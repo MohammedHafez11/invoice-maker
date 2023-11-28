ea9e492d11cb561050b4965a4e432e00a369a919
MohammedHafez11 <mohammedhafez45787@gmail.com>
2023-11-29 01:15:36 +0200

    first commit

---
+++ src/pages/about/AboutScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PageTitle from "../../components/Common/PageTitle";
+
+function AboutScreen() {
+  return (
+    <div>
+      <div className="p-4">
+        <div className="bg-white rounded-xl p-3 font-title">
+          <PageTitle title="About Me" />
+          <div className="mt-4 mb-5 flex flex-row items-center">
+            
+            <div>
+              <a
+                href="#"
+                target={"_blank"}
+                className="underline cursor-pointer"
+                rel="noreferrer"
+              >
+                Mohammed_Hafez
+              </a>
+              <h1> Free Invoice Maker for your business.</h1>
+            </div>
+          </div>
+
+          <PageTitle title="Invoice Maker" />
+          <div className="mt-2 pl-4 text-sm">
+            <ul class="list-disc">
+              <li> Can Easily Pre-Manage Your Products</li>
+              <li> Can Easily Pre-Manage Your Clients</li>
+              <li> Can Export PDF </li>
+              <li> Can Export Image </li>
+            </ul>
+          </div>
+          
+
+          <PageTitle title="Build By" />
+          <div className="mt-2 mb-5 pl-4 text-sm">
+            <ul class="list-disc">
+              <li> Framer Motion For each component Animation</li>
+              <li> Lottiefiles For Dashboard Widgets Icons</li>
+              <li> IndexedDB for Local Storage </li>
+              <li> ReactJS </li>
+            </ul>
+          </div>
+
+          <PageTitle title="Contact" />
+
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AboutScreen;
